f10da5332ce902b40facf6ab4772ac8a0c3c02a8
Thongtran98 <thongtran.kaka@gmail.com>
2023-07-31 14:14:12 +0700

    first commit

---
+++ force-app/main/default/aura/AddMultipleBill/AddMultipleBillController.js
@@ -0,0 +1,82 @@
+({
+    doinit: function (component,event,helper){
+      $A.enqueueAction(component.get('c.addRow'));
+    },
+    addRow: function(component, event, helper) {
+        //get the account List from component  
+        var billList = component.get("v.billList");
+        //Add New Account Record
+        billList.push({
+            'sobjectType': 'clofor_com_cfs__AnkenMeisai__c',
+            'clofor_com_cfs__Tariff__c': '',
+            'clofor_com_cfs__Suryo__c': '',
+            'clofor_com_cfs__ChargeUnit__c': '',
+            'clofor_com_cfs__Description__c': '', 
+            'clofor_com_cfs__Seikyusaki__c': '', 
+            'clofor_com_cfs__SellTankaJPY__c': '', 
+            'clofor_com_cfs__curr__c': '', 
+            'clofor_com_cfs__TaxInitial__c': '', 
+            'clofor_com_cfs__PaymentTo__c': '', 
+            'clofor_com_cfs__BuyTankaJPY__c': '', 
+            'clofor_com_cfs__CurrencyBuying__c': '', 
+            'clofor_com_cfs__BuyTaxInitial__c': '', 
+            
+        });
+        component.set("v.billList", billList);
+        console.log('billList:' + JSON.stringify(billList));
+
+    },
+    
+    removeRecord: function(component, event, helper) {
+        //Get the account list
+        var billList = component.get("v.billList");
+        //Get the target object
+        var selectedItem = event.currentTarget;
+        //Get the selected item index
+        var index = selectedItem.dataset.record;
+        //Remove single record from account list
+        billList.splice(index, 1);
+        //Set modified account list
+        component.set("v.billList", billList);
+    },
+    	
+    saveBills: function(component, event, helper) {      
+        if (helper.validateAccountRecords(component, event)) {
+            //Call Apex method and pass account list as a parameters
+            var spinner = component.find("spinner");
+            $A.util.toggleClass(spinner, "slds-hide");
+            var con = component.get("v.recordId");
+            var action = component.get("c.saveBillList");
+            action.setParams({
+                "billList": component.get("v.billList"),
+                "shipmentId": component.get("v.recordId"),
+            });
+
+            console.log('action:' + JSON.stringify(action));
+            console.log('action:' + JSON.stringify(con));
+
+            action.setCallback(this, function(response) {
+                //get response status 
+                var state = response.getState();
+                if (state === "SUCCESS") {
+                    //set empty account list
+                    component.set("v.billList", []);
+                    $A.util.toggleClass(spinner, "slds-hide");
+                    alert('Bill saved successfully');
+                    $A.enqueueAction(component.get('c.addRow'));
+
+                }
+            }); 
+            console.log('action:' + JSON.stringify(action));
+            $A.enqueueAction(action);
+            var url = "/lightning/r/clofor_com_cfs__CustomObject1__c/" + component.get("v.recordId")+"/view";
+            window.open(url, "_self");
+            // $A.util.toggleClass(spinner, "slds-hide");
+
+       }
+    },
+    onToggleSpinner: function(cmp){
+        var spinner = cmp.find("spinner");
+        $A.util.toggleClass(spinner, "slds-hide");
+    },
+})
---
+++ force-app/main/default/aura/AddMultipleQuoteLine/AddMultipleQuoteLineController.js
@@ -0,0 +1,66 @@
+({
+    doinit: function (component, event, helper) {
+      $A.enqueueAction(component.get('c.addRow'));
+    },
+  
+    addRow: function (component, event, helper) {
+      var lstObject = component.get("v.lstObject");
+
+          lstObject.push({
+            sobjectType: "clofor_com_cfs__QuoteLine__c",
+            clofor_com_cfs__PrintingSection__c: "",
+            clofor_com_cfs__ServicePricingID__c: "",
+            clofor_com_cfs__Print__c: true,
+            clofor_com_cfs__Quantity__c: "",
+            clofor_com_cfs__Unit__c: "",
+            clofor_com_cfs__ContainerType__c: "",
+            clofor_com_cfs__CurrencySelling__c: "",
+            clofor_com_cfs__TaxInitial__c: "",
+            clofor_com_cfs__SellingListPriceLocal__c: "",
+            clofor_com_cfs__SellingListPriceUSD__c: "",
+            clofor_com_cfs__BuyTaxInitial__c: "",
+            clofor_com_cfs__Remarks__c: ""
+          });
+          component.set("v.lstObject", lstObject);
+      console.log(JSON.stringify(lstObject))
+    },
+  
+    removeRecord: function (component, event, helper) {
+      var lstObject = component.get("v.lstObject");
+      var selectedItem = event.currentTarget;
+      var index = selectedItem.dataset.record;
+      lstObject.splice(index, 1);
+      component.set("v.lstObject", lstObject);
+    },
+  
+    saveBills: function (component, event, helper) {
+      // if (helper.validateAccountRecords(component, event)) {
+        var spinner = component.find("spinner");
+        $A.util.toggleClass(spinner, "slds-hide");
+  
+        var action = component.get("c.saveQuoteLineList");
+        action.setParams({
+          quoteLineList: component.get("v.lstObject"),
+          quoteId: component.get("v.recordId")
+        });
+  
+        action.setCallback(this, function (response) {
+          var state = response.getState();
+          if (state === "SUCCESS") {
+            component.set("v.lstObject", []);
+            $A.util.toggleClass(spinner, "slds-hide");
+            alert("Saved successfully");
+            $A.enqueueAction(component.get('c.addRow'));
+            var url = "/lightning/r/clofor_com_cfs__Quote__c/" + component.get("v.recordId")+"/view";
+            window.open(url, "_self");
+  
+          }
+        });
+        $A.enqueueAction(action);
+      // }
+    },
+    onToggleSpinner: function (cmp) {
+      var spinner = cmp.find("spinner");
+      $A.util.toggleClass(spinner, "slds-hide");
+    }
+  });
